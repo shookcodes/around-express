168ecd30ae4e388eb4144ebc6a3e62cc5ea78d8c
Sarah Shook <srowlesdeveloper@gmail.com>
2021-05-23 14:33:59 -0500

    updated route

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,6 @@ const { getUsers, createUser, patchUser } = require('../controllers/users');
 
 userRouter.get('/', getUsers);
 userRouter.post('/', createUser);
-userRouter.patch('/', patchUser);
+userRouter.patch('/:id', patchUser);
 
 module.exports = userRouter;
